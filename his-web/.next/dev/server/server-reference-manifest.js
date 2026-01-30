e985d31ffe5bbfa7fee03a7e75daf857d2a59c5a
sanyath007 <sanyath007@gmail.com>
2026-01-30 10:20:54 +0700

    build: add .gitignore file and .env.example instead of .env.local and remove sensitive file and folder from his-web

--- his-web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+DjhIi0zz0ZYEoXacNn9KKFhY8aEd0qNomurpNLpJj4=\"\n}"
--- his-web/.next/dev/static/chunks/his-web_src_app_settings_kiosk-menus_page_tsx_f254c265._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/his-web_src_9d1e6aaa._.js",
-  "static/chunks/c0d8b_d1698854._.js"
-],
-    source: "dynamic"
-});
